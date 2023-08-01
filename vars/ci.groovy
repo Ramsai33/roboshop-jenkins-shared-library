c87356df32516f83b49b1015bf477beacb9e46d9
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-08-01 14:12:56 +0530

    Jenkins

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -26,7 +26,10 @@ def call() {
 
                 stage('Quality control') {
                     steps {
-                        echo 'Quality control'
+                        SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                        SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+
+                        sh'sonar-scanner -Dsonar.host.url=http://172.31.82.44:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
                     }
                 }
 
